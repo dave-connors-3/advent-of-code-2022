e9028fd199a8019b8f6b87802ccac7ed00900024
Dave Connors <dave.connors@fishtownanalytics.com>
2023-04-09 20:33:48 -0500

    adjust test

--- tests/day_3.py
+++ tests/day_3.py
@@ -10,7 +10,12 @@ CrZsJsPPZsGzwwsLwLmpwMDw"""
 
 def test__error_sum():
     test_rucksacks = test_lines.split('\n')
-    print(test_rucksacks)
+    assert (day_3.get_error_priority(test_rucksacks)) == 157
+
+def test__badge_sum():
+    test_rucksacks = test_lines.split('\n')
+    assert (day_3.get_badge_priority(test_rucksacks)) == 70
+
 
 if __name__ == '__main__':
     test_rucksacks = test_lines.split('\n')
